4f54cbf7dbec64c950f45d6a0cbebdae934d1416
metinatmaca <atmacam16@itu.edu.tr>
2021-01-18 22:38:13 +0300

    t

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def submit():
 			return render_template('review.html', message='Please enter required fields',companies = companies_ , products = products_)
 		#todo: check if exists
 		cur.execute("SELECT product_id FROM products WHERE productname LIKE \'{0}\'".format(product))
-		p_id = cur.fetchall()[0][0]
+		p_id = cur.fetchall()[0]
 		cur.execute("INSERT INTO reviews(reviewcomment,reviewscore,product_id,user_id) VALUES ('{0}','{1}','{2}','{3}')".format(review,score,p_id,user_id))
 		con.commit()
 		cur.execute("SELECT review_id FROM reviews WHERE reviewcomment LIKE '{0}' AND product_id = '{1}' AND user_id = '{2}'".format(review,p_id[0][0],user_id))
