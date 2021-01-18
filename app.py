8f141e5cd7145491753974964d857e8395df638a
metinatmaca <atmacam16@itu.edu.tr>
2021-01-18 23:32:29 +0300

    t

--- app.py
+++ app.py
@@ -101,6 +101,7 @@ def submit():
 		#todo: check if exists
 		cur.execute("SELECT product_id FROM products WHERE productname LIKE '{0}'".format(product))
 		p_id = cur.fetchall()
+		global user_id
 		cur.execute("INSERT INTO reviews(reviewcomment,reviewscore,product_id,user_id) VALUES ('{0}','{1}','{2}','{3}')".format(review,score,p_id[0][0],user_id))
 		con.commit()
 		cur.execute("SELECT review_id FROM reviews WHERE reviewcomment LIKE '{0}' AND product_id = '{1}' AND user_id = '{2}'".format(review,p_id[0][0],user_id))
